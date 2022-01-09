a221334d702175330f35b2633ad0f16adcd79a28
Joshua Goble <gobleanimation@gmail.com>
2022-01-09 12:45:22 -0700

    Add TabViews

--- tabviewChallenge/ContentView.swift
+++ tabviewChallenge/ContentView.swift
@@ -8,10 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tabIndex = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        
+        TabView (selection: $tabIndex){
+            
+            // MARK: Tab 1
+            Text("This tab's tag is " + String(tabIndex))
+                
+                .tabItem {
+                    VStack{
+                        Image(systemName: "allergens")
+                    Text("Tab 1")
+                    }
+                }
+                .tag(0)
+            
+            // MARK: Tab 2
+            Button {
+                tabIndex = 2
+            } label: {
+                Text("Take me to tab 3")
+            }
+            
+            .tabItem {
+                VStack {
+                    Image(systemName: "arrow.uturn.right")
+                    Text("Tab 2")
+                }
+            }
+            .tag(1)
+            
+            // MARK: Tab 3
+            VStack {
+                List {
+                    ForEach (0..<100) { _ in
+                        Text("This is tab 3")
+                    }
+                }
+            }
+            .tabItem {
+                VStack {
+                    Image(systemName: "airpodspro")
+                    Text("Tab 3")
+                }
+            }
+            .tag(2)
+            
+
+            
+        }
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
