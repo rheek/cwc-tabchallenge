30219fb057a5513d97cc3fb161b30c4886bef94f
Joshua Goble <gobleanimation@gmail.com>
2022-01-09 10:52:32 -0700

    Initial Commit

---
+++ tabviewChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tabviewChallenge
+//
+//  Created by Joshua Goble on 1/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tabviewChallenge/tabviewChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabviewChallengeApp.swift
+//  tabviewChallenge
+//
+//  Created by Joshua Goble on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct tabviewChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
